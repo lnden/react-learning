ff18117a9a146ec561385c77aa5e2dce78d53e67
dinglindong <258231674@qq.com>
2018-11-14 15:46:23 +0800

    添加官方demo

---
+++ src/authority/demo.js
@@ -0,0 +1,8 @@
+/**
+ * @author Lnden
+ * @date 2018/11/14
+ * @desc 文件描述
+ * @param {Object} [title]  - 参数说明
+ * @example 调用示例
+ */
+
--- src/todolist/Table.js
+++ src/todolist/Table.js
@@ -1,5 +1,5 @@
 /**
- * @author Lindong Ding
+ * @author Lnden
  * @date 2018/11/14
  * @desc 文件描述
  * @param {Object} [title]  - 参数说明
