d1aa63540969a36ab2e0f210c30cb7f680cfc63d
dinglindong <258231674@qq.com>
2018-11-14 15:29:10 +0800

    Updates

---
+++ precache-manifest.af66c77fa181d9c937cdce43c66b8d95.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "acaf17d3bfda203f2deb",
+    "url": "/body1798/static/js/runtime~main.acaf17d3.js"
+  },
+  {
+    "revision": "4368442857c756c6c366",
+    "url": "/body1798/static/js/main.43684428.chunk.js"
+  },
+  {
+    "revision": "0cdd6f184db929cffcf5",
+    "url": "/body1798/static/js/1.0cdd6f18.chunk.js"
+  },
+  {
+    "revision": "4368442857c756c6c366",
+    "url": "/body1798/static/css/main.e443d715.chunk.css"
+  },
+  {
+    "revision": "089095fa39237593149a97754081fd28",
+    "url": "/body1798/index.html"
+  }
+];
