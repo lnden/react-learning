87121684991ce58168196bb29c0ba469888c4af7
dinglindong <258231674@qq.com>
2018-12-03 18:34:24 +0800

    使用iconfont图标

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -9,6 +9,7 @@
 import React, { Component } from 'react'
 import { NavLink } from 'react-router-dom'
 import './index.css'
+// require('//at.alicdn.com/t/font_774677_m4ixyv24l09.css')
 
 export default class CommFooter extends Component {
     render(){
@@ -16,16 +17,24 @@ export default class CommFooter extends Component {
             <article className="public-footer">
                 <ul>
                     <li>
-                        <NavLink to="/" exact activeClassName="selected">首页</NavLink>
+                        <NavLink to="/" exact activeClassName="selected">
+                            <i className="iconfont icon-_bendicang"></i>首页
+                        </NavLink>
                     </li>
                     <li>
-                        <NavLink to="/discover" activeClassName="selected">发现</NavLink>
+                        <NavLink to="/discover" activeClassName="selected">
+                            <i className="iconfont icon-_wyshenji"></i>发现
+                        </NavLink>
                     </li>
                     <li>
-                        <NavLink to="/message" activeClassName="selected">消息</NavLink>
+                        <NavLink to="/message" activeClassName="selected">
+                            <i className="iconfont icon-_muyue"></i>消息
+                        </NavLink>
                     </li>
                     <li>
-                        <NavLink to="/personal" activeClassName="selected">我的</NavLink>
+                        <NavLink to="/personal" activeClassName="selected">
+                            <i className="iconfont icon-_jinhuodl"></i>我的
+                        </NavLink>
                     </li>
                 </ul>
             </article>
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,12 @@
 /**
  * @author Lnden
  * @date 2018/11/21
- * @desc 文件描述
- * @param {Object} [title]  - 参数说明
- * @example 调用示例
+ * @desc 该组件使用iconfont的unicode方法使用
+ *
+ * unicode是字体在网页端最原始的应用方式，特点是：
+ *      兼容性最好，支持ie6+，及所有现代浏览器。
+ *      支持按字体的方式去动态调整图标大小，颜色等等。
+ *      但是因为是字体，所以不支持多色。只能使用平台里单色的图标，就算项目里有多色图标也会自动去色。
  */
 
 import React, { Component } from 'react';
@@ -16,43 +19,51 @@ const dataList = [
     {
         src:'/public',
         title:'笔记本',
-        img:'',
-        id:1
+        img:'&#xe605',
+        id:1,
+        color:'#D71920'
     }, {
         src:'/public',
         title:'信用卡还款',
-        img:'',
-        id:2
+        img:'&#xe601;',
+        id:2,
+        color:'#982C30'
     }, {
         src:'/public',
         title:'蚂蚁庄园',
-        img:'',
-        id:3
+        img:'&#xe60e;',
+        id:3,
+        color:'#2D2F64'
     }, {
         src:'/public',
         title:'蚂蚁森林',
-        img:'',
-        id:4
+        img:'&#xe602;',
+        id:4,
+        color:'#2D2F64'
     }, {
         src:'/public',
         title:'转账',
-        img:'',
-        id:5
+        img:'&#xe609;',
+        id:5,
+        color:'#263C73'
     }, {
         src:'/public',
         title:'充值中心',
-        img:'',
-        id:6
+        img:'&#xe606;',
+        id:6,
+        color:'#327341'
     }, {
         src:'/public',
         title:'我的快递',
-        img:'',
-        id:7
+        img:'&#xe617;',
+        id:7,
+        color:'#521C1D'
     }, {
         src:'/public',
         title:'运动',
-        img:'',
-        id:8
+        img:'&#xe615;',
+        id:8,
+        color:'#E9BC41'
     },
 ];
 class Home extends Component {
@@ -66,10 +77,7 @@ class Home extends Component {
                             return (
                                 <figure className="al-indexRecommendItem" key={val.id}>
                                     <Link to={val.src}>
-                                        {/*<img src="#" />*/}
-                                        <svg className="icon-svg" aria-hidden="true">
-                                            <use xlinkHref="#icon-Bank_of_Shanghai"></use>
-                                        </svg>
+                                        <i className="iconfont" style={{color:val.color}} dangerouslySetInnerHTML={{ __html: val.img }}></i>
                                         <figcaption>
                                             <span>{val.title}</span>
                                         </figcaption>
