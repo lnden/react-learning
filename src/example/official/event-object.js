f4126502e84f36d4d5715f5d861e0afaaa3327fc
ding lindong <258231674@qq.com>
2018-12-15 22:58:48 +0800

    add event

--- src/example/official/event-object.js
+++ src/example/official/event-object.js
@@ -16,6 +16,8 @@ class App extends Component{
         this.getData = this.getData.bind(this);
         this.handleChange = this.handleChange.bind(this);
         this.getInput = this.getInput.bind(this);
+        this.handleChangeRef = this.handleChangeRef.bind(this);
+        this.handleKeyup = this.handleKeyup.bind(this)
 
     }
 
@@ -24,7 +26,6 @@ class App extends Component{
         event.target.style.color='#fff';
         console.log("%c可以获取自定义属性：","color:#090",event.target.getAttribute('project'))
     }
-
     handleChange(event){
         this.setState({
             username:event.target.value
@@ -33,6 +34,19 @@ class App extends Component{
     getInput(){
         console.log(this.state.username)
     }
+    handleChangeRef(){
+        let value = this.refs.username.value;
+        this.setState({
+            username:value
+        })
+    }
+    handleKeyup(event){
+        if(event.keyCode===13){
+            console.log(event.target.value)
+        }
+    }
+
+
 
     render(){
         return (
@@ -44,12 +58,19 @@ class App extends Component{
 
                 <h3>表单事件</h3>
                 <input type="text" onChange={this.handleChange}/>
-                <button onClick={this.getInput}>获取input的值</button>
+                <button onClick={this.getInput}>事件对象获取input的值</button>
 
                 <hr/>
 
                 <h3>通过ref获取dom节点</h3>
-                
+                <input ref="username" type="text" onChange={this.handleChangeRef}/>
+                <button onClick={this.getInput}>ref获取input的值</button>
+
+                <hr/>
+
+                <h3>键盘事件</h3>
+                <input type="text" onKeyUp={this.handleKeyup}/>
+                <button>输入完成之后按回车键!</button>
             </section>
         )
     }
