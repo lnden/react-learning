479c49bc2f80e34ed0211080f23a1eaca381ba83
lnden <258231674@qq.com>
2019-05-04 23:07:39 +0800

    ADD STUDY DEMO

---
+++ src/study-demo/component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Comp extends Component {
+    render() {
+        return (
+            <section>
+                我是组件测试页面
+            </section>
+        )
+    }
+}
