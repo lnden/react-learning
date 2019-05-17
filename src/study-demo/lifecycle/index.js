2881cd07951beff098990151c3aa36c24754b8ec
lnden <258231674@qq.com>
2019-05-17 10:26:33 +0800

    add lifecycle

--- src/study-demo/lifecycle/index.js
+++ src/study-demo/lifecycle/index.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react'
 
+
+// React16.4版本生命周期
+
 class App extends Component {
     constructor(props) {
         super(props)
@@ -7,39 +10,57 @@ class App extends Component {
             name:'Lucy',
             flag:true
         }
-        console.log('%c constructor初始化执行','color:#f00')
+        console.log('%c constructor初始化执行1111111111111','color:#f00')
         this.handleClick = this.handleClick.bind(this)
         this.handleClickDestroy = this.handleClickDestroy.bind(this)
     }
 
     componentDidMount() {
-        console.log('%c componentDidMount安装执行','color:#00bcd4')
-    }
-
-    shouldComponentUpdate(nextProps,nextState) {
-        console.log('%c shouldComponentUpdate开始','color:#cddc39')
-        console.log(nextProps,1)
-        console.log(nextState,2)
-        console.log('%c shouldComponentUpdate结束','color:#cddc39')
-        return true
+        console.log('%c componentDidMount安装执行444444444444','color:#00bcd4')
     }
 
 
+
+    static getDerivedStateFromProps(props, state) {
+        console.log('！暂不关注该生命周期222222222222')
+        // console.log(props)
+        // console.log(state)
+        if(false){
+            return {
+                name: 'lnden',
+                flag: 'lable'
+            }
+        }
+        return null
+    }
     getSnapshotBeforeUpdate(prevProps, prevState) {
-        console.log(prevProps,prevState)
+        console.log('！暂不关注该生命周期666666666666')
+        // console.log(prevProps,prevState)
         return null
     }
 
-    componentDidUpdate() {
-        console.log('%c componentDidUpdate更新执行','color:#ff9800')
+
+
+
+
+    shouldComponentUpdate(nextProps,nextState) {
+        console.log('%c shouldComponentUpdate开始555555555555','color:#cddc39')
+        // console.log(nextProps,1)
+        // console.log(nextState,2)
+        console.log('%c shouldComponentUpdate结束555555555555','color:#cddc39')
+        return true
     }
+
+    componentDidUpdate() {
+        console.log('%c componentDidUpdate更新执行777777777777','color:#ff9800')
+    }
+
 
     handleClick() {
         this.setState({
             name: 'Tom'
         })
     }
-
     handleClickDestroy(event) {
         this.setState({
             flag: !this.state.flag
@@ -47,7 +68,7 @@ class App extends Component {
     }
 
     render() {
-        console.log('%c render渲染执行','background:#ccc;color:#000')
+        console.log('%c render渲染执行3333333333333','background:#ccc;color:#000')
         const STATUS = this.state.flag
         return (
             <section>
@@ -59,9 +80,7 @@ class App extends Component {
                         <Button handleClickDestroy={this.handleClickDestroy} />
                         :
                         ''
-
                 }
-
             </section>
         )
     }
@@ -71,8 +90,9 @@ class App extends Component {
 class Button extends Component {
 
     componentWillUnmount() {
-        console.log('%c componentWillUnmount组件卸载执行','background:#a00;color:#aaaaaa')
+        console.log('%c componentWillUnmount组件卸载执行888888888888','background:#a00;color:#aaaaaa')
     }
+
     render() {
         return (
             <button onClick={this.props.handleClickDestroy}>销毁组件</button>
