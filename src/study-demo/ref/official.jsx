d71cd3c89780ca4ff7d1acf0de0e1f199e70dae3
lnden <258231674@qq.com>
2019-05-27 10:47:09 +0800

    add official ref demo

--- src/study-demo/ref/official.jsx
+++ src/study-demo/ref/official.jsx
@@ -21,15 +21,19 @@ class MyComponent extends Component {
         )
     }
 }
-export default MyComponent
 
+
+//  为DOM元素添加ref属性 -----------------------------------------------------
 class CustomTextInput extends Component {
-    constructor() {
-
+    constructor(props) {
+        super(props)
+        this.textInput = React.createRef()
+        this.focusTextInput = this.focusTextInput.bind(this)
     }
 
     focusTextInput() {
-
+        //console.log('父亲组件执行了该方法')
+        this.textInput.current.focus()
     }
     
     render() {
@@ -37,8 +41,52 @@ class CustomTextInput extends Component {
             <section>
                 <input type="text" ref={this.textInput}/>
                 <br/>
-                <input type="text"       />
+                <input type="button" value="Focus the text input" onClick={this.focusTextInput} />
             </section>
         )
     }
 }
+
+
+//  为class组件添加ref属性 -----------------------------------------------------
+class  AutoFocusTextInput extends Component {
+    constructor(props) {
+        super(props)
+        this.textInput = React.createRef();
+    }
+
+    componentDidMount() {
+        //console.log(this.textInput.current)
+        this.textInput.current.focusTextInput()
+    }
+
+    render() {
+        return (
+            <CustomTextInput ref={this.textInput}/>
+        )
+    }
+}
+
+
+//  ref与函数组件、不能在函数组件上使用ref属性！--------------------------------------------------
+function MyFunctionComponent() {
+    return <input />
+}
+
+class Parent extends Component {
+    constructor(props) {
+        super(props)
+        this.textInput = React.createRef()
+    }
+    render() {
+        //  This will *not* work!
+        return (
+            <section>
+                <MyFunctionComponent ref={this.textInput} />
+            </section>
+        )
+    }
+}
+// export default MyComponent
+// export default CustomTextInput
+export default AutoFocusTextInput
