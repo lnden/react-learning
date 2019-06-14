5926baf5f8fe84b05299d4c5e11ce5c9352d7ac8
lnden <258231674@qq.com>
2019-06-14 18:34:57 +0800

    add redux

---
+++ src/study-demo/redux/redux-thunk.jsx
@@ -0,0 +1,84 @@
+import React, { Component } from 'react'
+import { createStore,applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import logger from 'redux-logger'
+
+const action = {
+    type: 'ADD_COUNT',
+    payload: 'add number'
+}
+const defaultStatus = 0;
+const reducer = (state = defaultStatus,action) => {
+    switch(action.type) {
+        case 'ADD_COUNT':
+            return state + 1
+        default:
+            return state
+    }
+}
+const store = createStore(reducer,applyMiddleware(thunk))
+
+class App extends Component {
+    render() {
+        return (
+            <section>
+                <h1>React-thunk浅析</h1>
+                <Button />
+                <Viewbox />
+            </section>
+        )
+    }
+}
+
+class Button extends Component {
+
+    handleClick = (dispatch,getState) => {
+        fetch('http://localhost:1234/api/test/user/users').then(res => {
+            return res.json();
+        })
+        .then(data => {
+            console.log(111)
+            // store.dispatch(action)
+        })
+        .catch(e => {
+            console.log(e)
+            store.dispatch(action)
+        })
+
+    }
+
+    render() {
+        return (
+            <section>
+                <button onClick={this.handleClick}>测试按钮</button>
+            </section>
+        )
+    }
+}
+
+class Viewbox extends Component {
+
+    constructor(props){
+        super(props)
+        this.state = {
+            count: store.getState()
+        }
+    }
+
+    componentDidMount() {
+        store.subscribe(()=>{
+            this.setState({
+                count: store.getState()
+            })
+        })
+    }
+
+    render() {
+        return (
+            <section>
+                <p>我应该是纯展示的数值：{this.state.count}</p>
+            </section>
+        )
+    }
+}
+export default App
