6d1c8acf87e7964d3716b7715d9c41cc91e11ba9
lnden <258231674@qq.com>
2019-06-19 14:20:28 +0800

    change combineReducers

--- src/store/merge.js
+++ src/store/merge.js
@@ -1,19 +1,34 @@
 /**
  * @author Lnden
  * @date 2018/11/30
- * @desc redux assemble the is file
- * @param {Object} [title]  - 参数说明
- * @example 调用示例
+ * @desc 该文件主要使用combineReducers合并reucer
  */
 
 
-import { createStore } from "redux";
-import { combineReducers } from 'redux';
+import { createStore, combineReducers } from "redux";
 
+
+//  创建第一个reducer
 const productsReducer = function(state=[], action) {
     return state;
 };
 
+//  创建第二个reducer
+const cartReducer = function(state=initialState, action) {
+    switch (action.type) {
+        case ADD_TO_CART: {
+            return {
+                ...state,
+                cart: [...state.cart, action.payload]
+            }
+        }
+
+        default:
+            return state;
+    }
+}
+
+//  初始化state
 const initialState = {
     cart: [
         {
@@ -29,22 +44,13 @@ const initialState = {
     ]
 }
 
+
+//  定义常量 [应该引入外部文件]
 const ADD_TO_CART = 'ADD_TO_CART';
 
-const cartReducer = function(state=initialState, action) {
-    switch (action.type) {
-        case ADD_TO_CART: {
-            return {
-                ...state,
-                cart: [...state.cart, action.payload]
-            }
-        }
 
-        default:
-            return state;
-    }
-}
 
+//  创建Action Creator
 function addToCart(product, quantity, unitCost) {
     return {
         type: ADD_TO_CART,
@@ -56,19 +62,22 @@ function addToCart(product, quantity, unitCost) {
     }
 }
 
+//  合并对象
 const allReducers = {
     products: productsReducer,
     shoppingCart: cartReducer
 }
-
+//  使用combineReducers合并对象
 const rootReducer = combineReducers(allReducers);
 
-let store = createStore(rootReducer);
 
-// console.log("initial state: ", store.getState());
+
+
+let store = createStore(rootReducer);
+console.log("initial state: ", store.getState());
 
 let unsubscribe = store.subscribe(() =>{
-    // console.log(store.getState())
+    console.log(store.getState())
 });
 
 store.dispatch(addToCart('Coffee 500gm', 1, 250));
