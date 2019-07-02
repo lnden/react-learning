a9801d5048a7a53f7e4e21505b41a21b37f9bb27
lnden <258231674@qq.com>
2019-07-02 17:21:14 +0800

    add @  paths.appSrc

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -20,12 +20,13 @@ export default class PublicHeader extends Component{
   toggleNav = () => {
     this.setState({navState: !this.state.navState});
   }
+
   // css动画组件设置为目标组件
   FirstChild = props => {
     const childrenArray = React.Children.toArray(props.children);
-    console.log(props.children,11)
     return childrenArray[0] || null;
   }
+
   shouldComponentUpdate(nextProps, nextState) {
     return !is(fromJS(this.props), fromJS(nextProps))|| !is(fromJS(this.state),fromJS(nextState))
   }
@@ -36,10 +37,10 @@ export default class PublicHeader extends Component{
         <span className="header-slide-icon icon-catalog" onClick={this.toggleNav}></span>
         <span className="header-title">{this.props.title}</span>
         {
-          this.props.record&&<NavLink to="/record" exact className="header-link icon-jilu">1111</NavLink>
+          this.props.record&&<NavLink to="/record" exact className="header-link icon-jilu"></NavLink>
         }
         {
-          this.props.confirm&&<NavLink to="/" exact className="header-link header-link-confim">确定222</NavLink>
+          this.props.confirm&&<NavLink to="/" exact className="header-link header-link-confim">确定</NavLink>
         }
         <ReactCSSTransitionGroup
           component={this.FirstChild}
@@ -47,7 +48,8 @@ export default class PublicHeader extends Component{
           transitionEnterTimeout={300}
           transitionLeaveTimeout={300}>
             {
-              this.state.navState && <aside key='nav-slide' className="nav-slide-list" onClick={this.toggleNav}>
+              this.state.navState &&
+               <aside key='nav-slide' className="nav-slide-list" onClick={this.toggleNav}>
                 <NavLink to="/" exact className="nav-link icon-jiantou-copy-copy">首页</NavLink>
                 <NavLink to="/balance" exact className="nav-link icon-jiantou-copy-copy">提现</NavLink>
                 <NavLink to="/helpcenter" exact className="nav-link icon-jiantou-copy-copy">帮助中心</NavLink>
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,12 +1,30 @@
 import React, { Component } from 'react'
-import PublicHeader from '../../components/header/index.jsx';
+import PublicHeader from '@/components/header/index.jsx';
 // import './index.less'
+// import mixin, { padStr } from '../../utils/mixin';
 
+// @mixin({padStr})
 class Home extends Component {
+
     render() {
         return (
             <main className="home-container">
                 <PublicHeader title='首页' record />
+                <p className="common-title">请录入您222的信息</p>
+                <form className="home-form">
+                    <div className="home-form-tiem">
+                        <span>销售金额：</span>
+                        <input type="text" placeholder="请输入订单金额" />
+                    </div>
+                    <div className="home-form-tiem">
+                        <span>客户姓名：</span>
+                        <input type="text" placeholder="请输入客户姓名" />
+                    </div>
+                    <div className="home-form-tiem">
+                        <span>客户电话：</span>
+                        <input type="text" maxLength="13" placeholder="请输入客户电话" />
+                    </div>
+                </form>
             </main>
         )
     }
