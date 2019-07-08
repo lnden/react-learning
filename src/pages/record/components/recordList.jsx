c8ee308a7651a3f8d57e82a46587d692c0d58aa7
lnden <258231674@qq.com>
2019-07-08 18:27:16 +0800

    add record pages

---
+++ src/pages/record/components/recordList.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './recordList.less'
+
+class RecordList extends Component {
+    render() {
+        return (
+            <div>
+                测试！
+            </div>
+        )
+    }
+}
+
+export default RecordList;
--- src/pages/record/record.jsx
+++ src/pages/record/record.jsx
@@ -1,12 +1,50 @@
 import React, { Component } from 'react'
+import { NavLink, Switch, Route, Redirect } from 'react-router-dom'
 import PublicHeader from '@/components/header/index.jsx'
+import RecordList from './components/recordList.jsx'
+import './record.less' 
 
 class Record extends Component {
+    state = {
+        flagBarPos: '17%'
+    }
+
+    setFlagBarPos = type => {
+        let flagBarPos;
+        switch(type){
+            case 'passed':
+                flagBarPos = '17%';break;
+            case 'audited':
+                flagBarPos = '50%';break;
+            case 'failed':
+                flagBarPos = '83%';break;
+            default :
+                flagBarPos = '17%';break;
+        }
+        this.setState({flagBarPos})
+    }
+
+    componentWillMount(){
+        let type = this.props.location.pathname.split('/')[2];
+        this.setFlagBarPos(type)
+    }
+
     render() {
         return (
-            <main>
+            <main className="common-con-top">
                 <PublicHeader title="记录" />
-                Record 
+                <section className="record-nav-con">
+                    <nav className="record-nav">
+                        <NavLink to={`${this.props.match.path}/passed`} className="nav-link">已通过</NavLink>
+                        <NavLink to={`${this.props.match.path}/audited`} className="nav-link">待审核</NavLink>
+                        <NavLink to={`${this.props.match.path}/failed`} className="nav-link">未通过</NavLink>
+                    </nav>
+                    <i className="nav-flag-bar" style={{left: this.state.flagBarPos}}></i>
+                </section>
+                <Switch>
+                    <Route path={`${this.props.match.path}/:type`} component={RecordList} />
+                    <Redirect from={`${this.props.match.path}`} to={`${this.props.match.path}/passed`} exact component={RecordList} />
+                </Switch>
             </main>
         )
     }
